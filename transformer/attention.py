16dcb99830e83be2fe41d9b3c9cbbfd176663c0a
Vaidehi Venkatesan <vaidehi.venkatesan@gmail.com>
2025-08-22 18:21:40 -0400

    Added SDPA

--- transformer/attention.py
+++ transformer/attention.py
@@ -9,6 +9,16 @@ Multi-head self-attention mechanism for transformer models.
 import torch 
 import math 
 
+def scaled_dot_product_attention(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor)->torch.Tensor: 
+    sqrt_d =  math.sqrt(q.size(-1))
+    scores = q @ k.transpose(-2, -1) # bs, n_heads, seq_len, head_dim @ bs, n_heads, head_dim, seq_len
+    scores = scores/sqrt_d # bs, n_heads, seq_len, seq_len
+    scores = torch.softmax(scores, dim=-1) # compute softmax scores along the last dim 
+
+    attn_out = scores @ v # bs, n_heads, seq_len, seq_len @ bs, n_heads, seq_len, head_dim --> bs, n_heads, seq_len, head_dim
+    attn_out = attn_out.transpose(1, 2) # bs, seq_len, n_heads, head_dim 
+    return attn_out
+
 class MultiHeadAttention(torch.nn.Module):
     """Multi-head self-attention with scaled dot-product attention."""
     def __init__(self, embed_dim: int, num_heads: int): 
@@ -32,14 +42,8 @@ class MultiHeadAttention(torch.nn.Module):
         v = self.v_proj(input).reshape([input.size(0), input.size(1), self.num_heads, self.head_dim]).permute(0,2,1,3)
 
         # scaled dot-product attention 
-        scores = q @ k.transpose(-2, -1) # bs, n_heads, seq_len, head_dim @ bs, n_heads, head_dim, seq_len
-        scores = scores/self.sqrt_d # bs, n_heads, seq_len, seq_len
-        scores = torch.softmax(scores, dim=-1) # compute softmax scores along the last dim 
-
-        attn_out = scores @ v # bs, n_heads, seq_len, seq_len @ bs, n_heads, seq_len, head_dim --> bs, n_heads, seq_len, head_dim
-        attn_out = attn_out.transpose(1, 2) # bs, seq_len, n_heads, head_dim 
+        attn_out = scaled_dot_product_attention(q, k, v)
         attn_out = attn_out.reshape([input.size(0), input.size(1), self.embed_dim])
-
         return self.out_proj(attn_out)
 
 
